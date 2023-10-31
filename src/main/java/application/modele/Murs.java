4f33f932138a7f02aede5059520943a7c28c7f9b
Heiwa70 <clement70200@gmail.com>
2023-10-31 17:33:30 +0100

    Ajouté du corps des vues Diag de class V1

--- src/main/java/application/modele/Murs.java
+++ src/main/java/application/modele/Murs.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class Murs {
     private ArrayList<Emplacement> casesPrisent;
 
-    public Murs(){
+    public Murs() {
         this.casesPrisent = new ArrayList<>();
     }
 
-    public void setPosition(Emplacement case1, Emplacement case2){
+    public void setPosition(Emplacement case1, Emplacement case2) {
 
         this.casesPrisent.add(case1);
         this.casesPrisent.add(case2);
