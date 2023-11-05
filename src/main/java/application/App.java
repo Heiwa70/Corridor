ba41d58b145bef5d38cdec3cbf5ab2f530e77936
Heiwa70 <clement70200@gmail.com>
2023-11-05 16:27:30 +0100

    V1 de la vue Home

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,8 +1,10 @@
 package application;
 
+import application.vue.pages.Home;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,12 +17,18 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-        var javaVersion = SystemInfo.javaVersion();
+       /* var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
+        stage.show();*/
+
+        Scene homeView = new Scene(new Home(40), 1280, 960);
+        stage.setScene(homeView);
+        stage.setTitle("Quoridor");
+        stage.getIcons().add(new Image("file:src/main/ressources/pictures/icon.png"));
         stage.show();
     }
 
