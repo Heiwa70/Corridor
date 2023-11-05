e9c92d7176c7c49f51934315dda2b7cb3144564d
nicochess90 <116639206+nicochess90@users.noreply.github.com>
2023-11-05 17:48:08 +0100

    fin de partie

--- src/main/java/application/controleur/Jeu.java
+++ src/main/java/application/controleur/Jeu.java
@@ -9,6 +9,7 @@ import application.modele.Emplacement;
 import application.modele.GestionSauvegardes;
 import application.modele.Joueur;
 import application.modele.Val;
+import application.modele.Log;
 
 import java.util.*;
 
@@ -235,9 +236,17 @@ public class Jeu {
     }
 
     public boolean finPartie() {
+        for (Joueur j : listeJoueurs) {
+            if ((j.getId() == 1 && j.getY() == 16)
+                    || (j.getId() == 2 && j.getY() == 0)
+                    || (j.getId() == 3 && j.getX() == 0)
+                    || (j.getId() == 4 && j.getX() == 16)) {
+                Log.info("FinDePartie", j.getNom() + " gagne, x:" + j.getX() + " y:" + j.getY());
+                return true;
+            }
+        }
         return false;
     }
-
     private void sauvegarde() {
         gestionSauvegardes.enregistrement(this.nomPartie, this.plateau, this.pointsJoueur, this.idJoueurActuel);
     }
